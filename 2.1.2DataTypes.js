036f807b2c6f5b4719e411d733ea1728644971b0
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-02-06 09:50:08 -0800

    initial commit

---
+++ 2.1.2DataTypes.js
@@ -0,0 +1,39 @@
+console.log(32);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+
+//Floating Point Number
+console.log(3.14);
+console.log(100.045);
+
+//Strings
+//Characters, letters, symbols, space
+console.log("hello world");
+console.log('hello world');
+console.log("hello world");
+
+//Arithmetic Operator (Math)
+console.log(14+2);
+console.log(12-2);
+console.log(14+2); //shift-8 gives us *
+console.log(14/2);
+console.log(14%2);// % is called MOD and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("My name"+" is Chuy.");
+console.log('My'+ ' ' + 'age' + ' '+'is'+' '+ 15 + '.');
+console.log("Your age is\n17."); // \n means new line
+
+//Template literal  - plug in a calculated value
+//use backticks `` next to the 1
+console.log(`15 divided by 10 = ${15/10} axcvzxcvx`);
+console.log(`12+5 = ${12+5}`);
+console.log(`101 divided by 5 has the remainder of: ${101%5}`);
+console.log(`Your age is ${14+3}`);
+console.log(`200 divided by 8 equals ${200/8}`);
